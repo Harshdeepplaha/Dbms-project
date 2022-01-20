ca94644f44877a9598041536685559563c9e1dc5
Harshdeep <harshdeepplaha05@gmail.com>
2022-01-20 17:15:26 +0530

    18jan added some ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LyCarouselModule } from '@alyle/ui/carousel';
+import { LyTypographyModule } from '@alyle/ui/typography';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), CommonModule,
+  LyCarouselModule,
+  LyTypographyModule],
+  exports: [RouterModule, CarouselExample01Component],
+  declarations: [CarouselExample01Component]
 })
 export class AppRoutingModule { }
+export class CarouselExample01Module { }
